9ce6b0745167e570ab239c01a54d42e9c84c5faa
t3kt <tommy.etkin@gmail.com>
2015-03-17 00:14:57 -0400

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+
+var config = {
+  cacheTimeout: 180000,
+  mongoOptions: {},
+  mongoUri: '',
+  registrationAllowed: false,
+  adminUser: null,
+  adminPass: null,
+  siteTitle: 'tekt',
+  siteAuthor: 'tekt'
+};
+
+_.merge(config, {
+  mongoUri: process.env.MONGOHQ_URL,
+  adminUser: process.env.FORMAGE_ADMIN_USER,
+  adminPass: process.env.FORMAGE_ADMIN_PASS
+});
+
+try
+{
+  _.merge(config, require('./config.private.json'));
+}
+catch (e)
+{
+  // do nothing
+}
+
+module.exports = config;
