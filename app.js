dcc8930742d1446598d4a8a39a0e6461349a6fa3
t3kt <tommy.etkin@gmail.com>
2015-03-20 20:13:09 -0400

    added 'dump' command

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ var program = require('commander'),
   core = require('./lib/core'),
   _ = require('lodash');
 
+function dumpMessageDetails(message, options) {
+  if (options.full) {
+    console.log(message.inspect());
+  } else if (options.showtext) {
+    _.forEach(message.getTextFragments(), function (textFrag) {
+      console.log('\t', textFrag);
+    });
+  }
+}
+
 program
   .command('list')
   .description('list messages in db')
@@ -20,17 +30,35 @@ program
         console.log('Found ' + messages.length + ' message(s):');
         _.forEach(messages, function (message, i) {
           console.log('[' + i + '] key: ' + message.key);
-          if (options.full) {
-            console.log(message.inspect());
-          } else if (options.showtext) {
-            _.forEach(message.getTextFragments(), function (textFrag) {
-              console.log('\t', textFrag);
-            });
-          }
+          dumpMessageDetails(message, options);
         });
       }
       process.exit(0);
     });
   });
+
+program
+  .command('dump <key>')
+  .description('view a single message by key')
+  .option('-f, --full', 'output full message json')
+  .option('-t, --showtext', 'show message text')
+  .action(function (key, options) {
+    options = _.extend({showtext: true}, options);
+    core.getMessage(key, function (err, message) {
+      if (err) {
+        console.error('Error loading message: ', err);
+        process.exit(1);
+      } else if (!message) {
+        console.error('Message not found: ', key);
+        process.exit(1);
+      } else {
+        if (!options.full)
+          console.log('Found message: ', key);
+        dumpMessageDetails(message, options);
+        process.exit(0);
+      }
+    })
+  });
+
 
 program.parse(process.argv);
