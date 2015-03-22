a5ea3c2bcf25a323a15d291332184c440bc2eb67
t3kt <tommy.etkin@gmail.com>
2015-03-22 16:19:45 -0400

    starting to implement twitter storage

--- config/config.js
+++ config/config.js
@@ -1,29 +1,23 @@
 var _ = require('lodash');
 
 var config = {
-  cacheTimeout: 180000,
   mongoOptions: {},
   mongoUri: '',
-  registrationAllowed: false,
-  adminUser: null,
-  adminPass: null,
-  siteTitle: 'tekt',
-  siteAuthor: 'tekt',
-  defaultMessageOptions: {}
+  defaultMessageOptions: {},
+  twitterConsumerKey: null,
+  twitterConsumerSecret: null,
+  twitterAccessTokenKey: null,
+  twitterAccessTokenSecret: null
 };
 
 _.merge(config, {
-  mongoUri: process.env.MONGOHQ_URL,
-  adminUser: process.env.FORMAGE_ADMIN_USER,
-  adminPass: process.env.FORMAGE_ADMIN_PASS
+  mongoUri: process.env.MONGOHQ_URL
 });
 
-try
-{
+try {
   _.merge(config, require('./config.private.json'));
 }
-catch (e)
-{
+catch (e) {
   // do nothing
 }
 
